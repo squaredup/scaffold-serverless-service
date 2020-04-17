6905fc3f2a9c4bd6cbaf49e6756a601377baa82b
Dave Clarke <dave.clarke@squaredup.com>
2020-04-17 17:16:13 +0100

    Add name argument

--- core.js
+++ core.js
@@ -9,21 +9,20 @@ const log = require('./log.js');
  * Creates a service using the template in the ./template folder
  * Also replaces placeholders in package.json etc.
  */
-const Scaffold = async () => {
-    // Get name from arguments
-    const name = (process.argv[2] || '').toLowerCase();
+const Scaffold = async (name) => {
+    const nameLower = name.toLowerCase();
 
     // Create the service using template
     const template = path.resolve(__dirname, "template");
-    exec(`serverless create --template-path "${template}" --name ${name}`, {stdio:[0, 1, 2]});
+    exec(`serverless create --template-path "${template}" --name ${nameLower}`, {stdio:[0, 1, 2]});
 
     // Replace tokens in package.json
-    const packagePath = path.resolve(process.cwd(), name, "package.json");
+    const packagePath = path.resolve(process.cwd(), nameLower, "package.json");
     const package = await util.promisify(fs.readFile)(packagePath, 'utf8');
-    const result = package.replace(/__SERVICE_NAME__/g, name);
+    const result = package.replace(/__SERVICE_NAME__/g, nameLower);
     await util.promisify(fs.writeFile)(packagePath, result, 'utf8');
 
-    log.ok(`Created new service at ${chalk.green.underline.bold(`${name}/serverless.yml`)}`);
+    log.ok(`Created new service at ${chalk.green.underline.bold(`${nameLower}/serverless.yml`)}`);
 }
 
 module.exports = { Scaffold };
